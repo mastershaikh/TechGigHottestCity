857584d94b7954e64848c7386041261fb2b37243
Nizamuddin <nizamuddin.shaikhh@gmail.com>
2019-03-09 12:54:40 +0530

    Master branch - first commit

---
+++ hot-city/src/main/java/com/techgig/hotcity/HotCityApplication.java
@@ -0,0 +1,13 @@
+package com.techgig.hotcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotCityApplication.class, args);
+	}
+
+}
